694a4de8b9f04a15911aa717f34a78cea21727b6
aanand-mohan <165379314+aanand-mohan@users.noreply.github.com>
2026-01-21 14:37:16 +0530

    Update api.js
    CHANGE API

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://e-comm-8ozk.onrender.com'
 });
 
 // Request interceptor to add auth token
