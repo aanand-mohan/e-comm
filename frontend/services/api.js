d1fbf030a978d23538e858a784863eca223583bb
DEVA SINGH RAJPUT <anandmoha.7970@gmail.com>
2026-01-29 12:07:51 +0530

    Refactor API service to use environment variable

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:  'https://e-comm-8ozk.onrender.com'
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'
 });
 
 // Request interceptor to add auth token
